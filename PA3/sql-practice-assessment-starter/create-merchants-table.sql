f8628d4ecfb5fa5c35a591184e3c5ff1aff42699
Exequiel <exequiel.hashimoto@outlook.com>
2022-05-22 15:57:55 -0700

    redo for rouben

--- PA3/sql-practice-assessment-starter/create-merchants-table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE merchants (
-    id SERIAL PRIMARY KEY NOT NULL,
-    merchant_name VARCHAR(255) NOT NULL,
-    country_id INTEGER NOT NULL REFERENCES countries (id),
-    created_at TIMESTAMP NOT NULL,
-    admin_id INTEGER NOT NULL REFERENCES users (id),
-    merchant_type_id INTEGER NOT NULL REFERENCES merchant_types (id)
-);
--- PA3/sql-practice-assessment-starter/joined-data-query.sql
+++
@@ -1,5 +0,0 @@
-SELECT full_name, type, name, merchant_name FROM merchants
-INNER JOIN merchant_types ON (merchants.merchant_type_id = merchant_types.id)
-INNER JOIN countries ON (merchants.country_id = countries.id)
-INNER JOIN users ON (merchants.admin_id = users.id)
-ORDER BY merchant_name;
